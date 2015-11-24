0e1c20dbaa6edd8d82f3253418f3e5c7b3f8d116
unknown <vseth968@FHS-LAB127-LE26.fhs.win.fuhsd.org>
2015-11-24 09:11:08 -0800

    changed Entity.java

---
+++ Entity.java
@@ -0,0 +1,50 @@
+
+public class Entity {
+	Location loc;
+	Room currentRoom;
+	
+
+	public Entity(Room r) {
+		currentRoom = r;
+		loc = r.getRandomLocation();
+	}
+
+	public Entity(Room r, Location l) {
+		currentRoom = r;
+		loc = l;
+		currentRoom.put(loc.row, loc.col, Game.EMPTY);
+	}
+	public Entity(Room r, Location l, int gameVar) {
+		currentRoom = r;
+		loc = l;
+		currentRoom.put(loc.row, loc.col, gameVar);
+	}
+	public Entity(Room r, int gameVar) {
+		currentRoom = r;
+		currentRoom.put(loc.row, loc.col, gameVar);
+	}
+	/*
+	public Entity(){
+	}*/
+
+	public Room getRoom() {
+		return currentRoom;
+	}
+
+	public void setRoom(Room r) {
+		currentRoom = r;
+	}
+
+	public Location getLocation() {
+		return loc;
+	}
+	
+	public void setLocation(Location loc) {
+		if(this.loc!=null)
+			currentRoom.put(this.loc.row, this.loc.col, Game.EMPTY);
+			
+		this.loc = new Location(loc.row, loc.col);
+		currentRoom.put(loc.row, loc.col, currentRoom.get(this.loc.row, this.loc.col));
+	}
+
+}
