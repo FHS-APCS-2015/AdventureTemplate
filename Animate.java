529d716f6e5bfc6d9cfb14b1bd43932e377c2a92
unknown <vseth968@FHS-LAB127-LE26.fhs.win.fuhsd.org>
2015-11-24 09:10:04 -0800

    changed Animate.java

---
+++ Animate.java
@@ -0,0 +1,54 @@
+
+public class Animate extends Entity {
+	boolean alive;
+
+	public Animate(Room r) {
+		super(r);
+	}
+
+	public Animate(Room r, Location loc) {
+		super(r, loc);
+	}
+	public Animate(Room r, Location loc, int gameVar) {
+		super(r, loc, gameVar);
+	}
+	public Animate(Room r, int gameVar) {
+		super(r, gameVar);
+	}
+
+	public void move(int direction) {
+		Location moveTo = Location.locationInDirection(loc, direction);
+
+		if (currentRoom.isEmpty(moveTo.row, moveTo.col)) {
+			currentRoom.moveElementAt(loc, direction);
+
+			loc = moveTo; // update own location
+		}
+	}
+
+	public void moveRandom() {
+		int random = (int) (Math.random() * 4);
+		move(random);
+	}
+
+	public void moveToward(Location loc) {
+		int direction = 0, greatestDist = 0;
+		if (loc.row > this.loc.row && Math.abs(loc.row - this.loc.row) > greatestDist) {
+			direction = Location.NORTH;
+			greatestDist = Math.abs(loc.row - this.loc.row);
+		}
+		if (loc.row < this.loc.row && Math.abs(loc.row - this.loc.row) > greatestDist) {
+			direction = Location.SOUTH;
+			greatestDist = Math.abs(loc.row - this.loc.row);
+		}
+		if (loc.col > this.loc.col && Math.abs(loc.col - this.loc.col) > greatestDist) {
+			direction = Location.EAST;
+			greatestDist = Math.abs(loc.col - this.loc.col);
+		}
+		if (loc.col < this.loc.col && Math.abs(loc.col - this.loc.col) > greatestDist) {
+			direction = Location.WEST;
+			greatestDist = Math.abs(loc.col - this.loc.col);
+		}
+		move(direction);
+	}
+}
