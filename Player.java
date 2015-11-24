4d25fd3be27fe25a4787f7b92611038a72f3c960
unknown <vseth968@FHS-LAB127-LE26.fhs.win.fuhsd.org>
2015-11-24 09:08:02 -0800

    changed Player.java

--- Player.java
+++ Player.java
@@ -3,27 +3,51 @@
  * 
  * @author David
  */
-public class Player {
-    private Room currentRoom;				// room player is in
-    private Location loc;					// location of the player in the room
-    
-    public Player(Room r) {
-        currentRoom = r;
-        loc = new Location(currentRoom.getHeight()/2, currentRoom.getWidth()/2);
-        currentRoom.put(loc.row, loc.col, Game.PLAYER);
-    }
-    
-    // returns true if player was able to move in that direction.
-    public boolean move(int direction) {
-        Location moveTo = Location.locationInDirection(loc, direction);
-        
-        if (currentRoom.isEmpty(moveTo.row, moveTo.col)) {
-            currentRoom.moveElementAt(loc, direction);
+public class Player extends Animate{
 
-            loc = moveTo;   // update own location
-            return true;
-        }
-        
-        return false;
-    }
+	public Player(Room r) {
+		super(r, Game.PLAYER);
+		loc = new Location(currentRoom.getHeight() / 2, currentRoom.getWidth() / 2);
+
+	}
+
+	public void moveToward(Entity e){
+		moveToward(e.loc);
+	}
+
+
+	// returns true if player was able to move in that direction.
+	public boolean ableToMove(int direction) {
+		Location moveTo = Location.locationInDirection(loc, direction);
+
+		if (currentRoom.isEmpty(moveTo.row, moveTo.col)) {
+			currentRoom.moveElementAt(loc, direction);
+
+			loc = moveTo; // update own location
+			return true;
+		}
+
+		return false;
+	}
+
+	public void escape() {
+		Location newLocation;
+
+		do {
+			newLocation = currentRoom.getRandomLocation();
+		} while (!currentRoom.isEmpty(newLocation));
+
+		currentRoom.put(loc.row, loc.col, Game.EMPTY);
+		currentRoom.put(newLocation.row, newLocation.col, Game.PLAYER);
+
+		this.loc = newLocation;
+
+	}
+
+	// returns sum of difference of rows and difference of columns
+	public int getDistanceTo(Wumpus w) {
+		int colSum = Math.abs(loc.col - w.getCol());
+		int rowSum = Math.abs(loc.row - w.getRow());
+		return colSum + rowSum;
+	}
 }
